53eecfc2033b65b31250dc5617d62ac9bd25fae7
vic85821 <vic85821@gmail.com>
2017-10-15 20:47:14 +0800

    add navbar and footer

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$("div > .navbar-nav > ul > li > a").hover(
+	function() {
+    $(this).css("color","black");
+  },
+  function() {
+    $(this).css("color","#888");
+ 	}
+);
+
+$("a.icon").hover(
+	function() {
+    $(this).css("color","#5599DD");
+  },
+  function() {
+    $(this).css("color","#CCCCCC");
+ 	}
+);
